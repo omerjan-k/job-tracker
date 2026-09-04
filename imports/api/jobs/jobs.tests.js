b93afd91293207dce99f447906205ea38a277360
omerjan-k <omerjan.k@gmail.com>
2026-09-04 20:45:07 +0200

    feat: enhance CI/CD pipeline with testing and deployment steps

---
+++ imports/api/jobs/jobs.tests.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import { assert } from 'chai';
+
+
+describe('job-tracker', function () {
+  it('sanity check - basic math', function () {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
